a7cbfe43be163250d4112542234a446acae60202
LucasFaletra <Lucas_Faletra@student.uml.edu>
2020-01-29 15:50:28 -0500

    quick add

---
+++ install/install.sh
@@ -0,0 +1,3 @@
+#this is going to be a shell script to install everything and quickly reconfigure my operating system
+#there is nothing here at the moment but I plan on creating this in the future.
+#for now this useless shell script file is all thats in this directory
